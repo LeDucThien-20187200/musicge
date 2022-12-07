0a2320ab90b779b466e768232a2c5ef0b49abdac
LeThienn <55796074+LeThienn@users.noreply.github.com>
2022-12-07 09:23:38 +0700

    7/12/2022-LeDucThien-update localization and lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,153 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:musify/API/musify.dart';
+import 'package:musify/helper/version.dart';
+import 'package:musify/services/audio_handler.dart';
+import 'package:musify/services/audio_manager.dart';
+import 'package:musify/services/data_manager.dart';
+import 'package:musify/style/appColors.dart';
+import 'package:musify/ui/rootPage.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+GetIt getIt = GetIt.instance;
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  static void setLocale(BuildContext context, Locale newLocale) async {
+    _MyAppState state = context.findAncestorStateOfType<_MyAppState>()!;
+    state.changeLanguage(newLocale);
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  Locale _locale = const Locale('en', '');
 
-  void _incrementCounter() {
+  changeLanguage(Locale locale) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _locale = locale;
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    final String lang =
+        Hive.box('settings').get('language', defaultValue: 'English') as String;
+    prefferedFileExtension.value = Hive.box('settings')
+        .get('audioFileType', defaultValue: 'mp3') as String;
+    final Map<String, String> codes = {
+      'English': 'en',
+      'Georgian': 'ka',
+      'Chinese': 'zh',
+      'Dutch': 'nl',
+      'German': 'de',
+      'Indonesian': 'id',
+      'Italian': 'it',
+      'Polish': 'pl',
+      'Portuguese': 'pt',
+      'Spanish': 'es',
+      'Turkish': 'tr',
+      'Ukrainian': 'uk',
+    };
+    _locale = Locale(codes[lang]!);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: accent,
+        scaffoldBackgroundColor: bgColor,
+        canvasColor: bgColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.ubuntuTextTheme(Theme.of(context).textTheme),
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: ZoomPageTransitionsBuilder(),
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''),
+        Locale('ka', ''),
+        Locale('zh', ''),
+        Locale('nl', ''),
+        Locale('fr', ''),
+        Locale('de', ''),
+        Locale('he', ''),
+        Locale('hi', ''),
+        Locale('hu', ''),
+        Locale('id', ''),
+        Locale('it', ''),
+        Locale('pl', ''),
+        Locale('pt', ''),
+        Locale('es', ''),
+        Locale('ta', ''),
+        Locale('tr', ''),
+        Locale('uk', ''),
+        Locale('ur', '')
+      ],
+      locale: _locale,
+      home: Musify(),
     );
   }
+}
+
+main() async {
+  await Hive.initFlutter();
+  await getLocalSongs();
+  await FlutterDownloader.initialize(
+    debug:
+        true, // optional: set to false to disable printing logs to console (default: true)
+    ignoreSsl:
+        true // option: set to false to disable working with http links (default: false)
+    ,
+  );
+  FlutterDownloader.registerCallback(TestClass.callback);
+  accent = await getData("settings", "accentColor") != null
+      ? Color(await getData("settings", "accentColor") as int)
+      : const Color(0xFFFF9E80);
+  userPlaylists = await getData("user", "playlists") ?? [];
+  userLikedSongsList = await getData("user", "likedSongs") ?? [];
+  final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  version = packageInfo.version;
+  await enableBooster();
+  initialisation();
+  runApp(MyApp());
+}
+
+Future<void> initialisation() async {
+  final AudioHandler audioHandler = await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'me.musify',
+      androidNotificationChannelName: 'Musify',
+      androidNotificationOngoing: true,
+      androidStopForegroundOnPause: true,
+      androidNotificationIcon: 'drawable/musify',
+      androidShowNotificationBadge: true,
+    ),
+  );
+  getIt.registerSingleton<AudioHandler>(audioHandler);
+}
+
+class TestClass {
+  static void callback(String id, DownloadTaskStatus status, int progress) {}
 }
